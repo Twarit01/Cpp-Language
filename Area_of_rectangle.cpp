f58379fa708970efd40668f719af12fcbd961218
Twarit01 <twaritshukla.01@gmail.com>
2025-02-06 00:13:49 +0530

    More Ques

---
+++ Area_of_rectangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int area(int,int);
+
+int main(){
+    int answer,l,b;
+    cout<<"Enter the length and bredth of number"<<endl;
+    cin>>l>>b;
+    answer=area(l,b);
+    cout<<"Area is "<<answer;
+    return 0;
+}
+int area(int x,int y)
+{
+    return x*y;
+}
